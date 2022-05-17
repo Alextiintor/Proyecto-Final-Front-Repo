dfa097b08fa50936a354ced36b251529a15e5798
Alex <agallardo@ibadia.cat>
2022-05-17 16:14:43 +0200

    proceso de crear homePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import Nav from './Nav.vue'
+</script>
+
+<template>
+  <Nav></Nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,72 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+      <div class="logo">
+          <i class="fa-solid fa-shuttle-space"></i>
+          <h1>GESTBOT</h1>
+      </div>
+      <div class="nav-links">
+          <a href="#">About</a>
+          <a href="#">About</a>
+          <a href="#">About</a>
+          <i class="fa-solid fa-earth-europe"></i>
+          <button class="btn"> ACCESS </button>
+      </div>
+  </nav>
+</template>
+
+<style scoped>
+    nav{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    h1{
+        font-family: "Nova Flat";
+    }
+
+    .logo{
+        display: flex;
+        align-items: center;
+    }
+
+    .nav-links{
+        display: flex;
+        align-items: center;
+    }
+
+    .nav-links a {
+        font-family: 'Josefin Sans';
+        font-size: 1.5rem;
+        color: black;
+        text-decoration: none;
+        margin-right: 35px;
+    }
+
+    .nav-links a:hover {
+        text-decoration: underline;
+    }
+
+    .nav-links i{
+        margin-right: 20px;
+    }
+
+    .btn{
+        border: none;
+        border-radius: 7px;
+        font-family: 'Josefin Sans';
+        font-size: 16px;
+        font-weight: bold;
+        padding: 12px 20px;
+        color: white;
+        background-color: #56A0D3;
+    }
+
+    .btn:hover{
+        background-color: #4987b0;
+    }
+</style>

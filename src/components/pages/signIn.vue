54aa3a56cc222afc5d01b65f95c82cd6eaf7a2e2
Oscar Adell <oadell@ibadia.cat>
2022-05-21 21:47:42 +0200

    signin done no funcional

--- src/components/pages/signIn.vue
+++ src/components/pages/signIn.vue
@@ -31,7 +31,7 @@
             <h1>Iniciar session</h1>
         </div>
         <form action="" method="post">
-            <div class="item-form">
+            <div class="item-form" style="margin-top: 2vh;">
                 <span for="">Email</span>
                 <input type="email" name="" id="">
             </div>
@@ -39,8 +39,12 @@
                 <span for="">Contraseña</span>
                 <input type="password" name="" id="">
             </div>
-            <input type="submit" value="Iniciar">
+            <input class="send-btn" style="margin-top: 2vh;" type="submit" value="Iniciar">
         </form>
+      </div>
+      <div class="signup">
+        <p>Si no tienes cuenta, dale aqui para crear una</p>
+        <button class="send-btn">Registrate</button>
       </div>
     <RoomCard 
       v-for="robot in robots"
@@ -53,49 +57,76 @@
 </template>
 
 <style scoped>
-    .signin{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .header-sign{
-        display: flex;
-        align-items: center;
-        
-        font-family: "Nova Flat";
-        font-size: 1.5rem;
-        font-weight: bold;
-        margin: 0;
-    }
-    .icon-container{
-        border-radius: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 60px;
-        height: 60px;
-        background-color: var(--blue);
-        margin-right: 1vw;
-    }
-    .icon-container i{
-        font-size: 2.5rem;
-        color: var(--white);
-    }
-    form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 40vw;
-    }
-    .item-form {
-    }
-    .item-form span{
-        width: 35vw;
-    }
-    span{
+  .container{
+    flex-direction: column;
+  }
+  .signin{
+    margin-top: 25vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+  }
+  .header-sign{
+      display: flex;
+      align-items: center;
+      
+      font-family: "Nova Flat";
+      font-size: 1.5rem;
+      font-weight: bold;
+      margin: 0;
+  }
+  .icon-container{
+      border-radius: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 60px;
+      height: 60px;
+      background-color: var(--blue);
+      margin-right: 1vw;
+  }
+  .icon-container i{
+      font-size: 2.5rem;
+      color: var(--white);
+  }
+  form{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 20vw;
+  }
+  .item-form {
+    margin-bottom: 1vh;
+  }
+  .item-form span{
+      width: 35vw;
+  }
+  span{
+    font-family: 'Asap';
+  }
+  input{
+    border: 1px solid var(--blue);
+    border-radius: 0.35vw;
+    width: 20vw;
+    height: 3vh;
+    background-color: var(--grey);
+  }
 
-    }
-    input{
+  .send-btn{
+    width: 12.5vw;
+    border: unset;
+    height: 6vh;
+    background-color: var(--blue);
+    color: var(--white);
+    font-family: "Nova Flat";
+    font-size: 34px;
+    border-radius: 0.3vw;
+  }
 
-    }
+  .signup{
+    margin-top: 15vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
